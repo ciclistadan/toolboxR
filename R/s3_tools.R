b8077d3b384c393819049289087a2326ba77a386
dkrozelle <dan.rozelle@gmail.com>
2017-03-08 15:39:57 -0500

    add Get/PutS3 table functions

--- R/s3_tools.R
+++ R/s3_tools.R
@@ -42,3 +42,32 @@ WriteTableToS3 <- function(bucket, path = "", arguments = ""){
 # 
 # 
 
+
+#' Read tables from S3 bucket directly into df object 
+#'
+#' Works for all delimited tables supported by toolboxR::Autoread().
+#' which includes csv, tab-delim, and xlsx). Only 
+#'
+#' @param s3.path Full character string uri of the table. 
+#' @param cache logical value as to whether local file should be 
+#'              left, default for cache=F removes file after transfer.
+#' @return data.frame
+#' @export
+GetS3Table <- function(s3.path, cache = F, ...){
+  name  <- basename(s3.path)
+  local.filename <- file.path("/tmp", name)
+  system(  paste('aws s3 cp', s3.path, local.filename, sep = " "))
+  df <- toolboxR::auto_read(local.filename, ...)
+  if(cache == FALSE){unlink(local.filename)}
+  df
+}
+
+#currently only works for tab-delim tables
+PutS3Table <- function(object, s3.path, cache = F){
+  name  <- basename(s3.path)
+  local <- file.path("/tmp", name)
+  write.table(object, local, row.names = F, quote = F, sep = "\t")
+  
+  system(  paste('aws s3 cp', local, s3.path, "--sse", sep = " "))
+  if(cache == FALSE){unlink(local)}
+}
