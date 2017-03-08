fa16aa28adbad1c1cfd59919fc2a1760bcad07d7
dkrozelle <dan.rozelle@gmail.com>
2017-03-08 15:39:33 -0500

    deprecate AutoRead() and add auto_read() which incorporated *dots* argument usage

--- R/file_io.R
+++ R/file_io.R
@@ -1,27 +1,45 @@
+#' Attempts to automatically recognize table type and import
+#'  as tibble. Latest version incorporates *dots* to facilitate
+#'  passing all possible options for each file reader type.
+#'
+#' @param file path to an xlsx binary file
+#' @param remove.empty.columns,remove.empty.rows logical to remove columns or
+#'            rows composed entirely of NA, "NA", or blank values
+#' @param ... Pass any file-type appropriate arguments on to their handler 
+#'            functions. e.g., readxl::read_excel(), read.delim(), read.csv()
+#' @return dataframe
+#' @export
+auto_read <- function(file, stringsAsFactors = F, remove.empty.columns = T, remove.empty.rows = T, ...){
+  
+  if(endsWith(file,"xlsx") | endsWith(file, "xls")){
+    df <- readxl::read_excel(file, ...)
+  }else if (endsWith(file, "txt") | endsWith(file, "tsv")) {
+    df <- dplyr::as.tbl(read.delim(file, stringsAsFactors = stringsAsFactors, ...))
+  }else if (endsWith(file, "csv") ) {
+    df <- dplyr::as.tbl(read.csv(file, stringsAsFactors = stringsAsFactors, ...))
+  }else{ stop("file was not automatically recognizable type (xlsx, xls, txt, csv)") }
+  
+  # remove empty rows and colums
+  if(remove.empty.columns) df <- df[, !apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )})]
+  if(remove.empty.rows)    df <- df[!apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )}), ] 
+  df
+}
 
+
+#' Deprecated, use auto_read(). 
 #' Attempts to automatically recognize file type and import
 #'  as dataframe
 #'
 #' @param file path to an xlsx binary file
-#' @param sheet number or name of workbook sheet to import.
-#'              Defaults to first sheet.
-#' @param check.names logical. If TRUE then the names of the variables in the 
-#'                    data frame are checked to ensure that they are syntactically 
-#'                    valid variable names. If necessary they are adjusted 
-#'                    (by make.names) so that they are, and also to ensure 
-#'                    that there are no duplicates.
-#' @param skip number of lines to skip before import.
+#' @param ... Pass any file-type appropriate arguments on to their handler 
+#'            functions. e.g., readxl::read_excel(), read.delim(), read.csv()
 #' @return dataframe
 #' @export
-AutoRead <- function(file, sheet = 1, check.names = T, skip = 0, colnames = T){
+AutoRead <- function(file, ...){
   tryCatch({
     if(endsWith(file,"xlsx") | endsWith(file, "xls")){
       t <- readxl:::xlsx_col_types(file)
-      df <- readxl::read_excel(path = file, 
-                               sheet = sheet, 
-                               skip = skip, 
-                               col_names = colnames,
-                               col_types = t )
+      df <- readxl::read_excel(file, ...)
       if(check.names){
         df <- df[,!is.na(names(df))]
         names(df) <- make.names(names(df), unique = TRUE)
