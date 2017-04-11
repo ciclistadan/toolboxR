b24cc17f5c4faabd6a5ad3a438aa378f15f38b5f
dkrozelle <dan.rozelle@gmail.com>
2017-04-11 14:39:43 -0400

    add reader option for auto_read

--- R/file_io.R
+++ R/file_io.R
@@ -3,25 +3,32 @@
 #'  passing all possible options for each file reader type.
 #'
 #' @param file path to an xlsx binary file
+#' @param reader Specify which package to read in your table, options include:
+#'              read_excel, read.delim, read.csv, fread. NOTE: fread option
+#'              will return a data.table, all others return data frame.
 #' @param remove.empty.columns,remove.empty.rows logical to remove columns or
 #'            rows composed entirely of NA, "NA", or blank values
 #' @param ... Pass any file-type appropriate arguments on to their handler 
 #'            functions. e.g., readxl::read_excel(), read.delim(), read.csv()
-#' @return dataframe
+#' @return dataframe or data.table, see reader parameter.
 #' @export
-auto_read <- function(file, stringsAsFactors = F, remove.empty.columns = T, remove.empty.rows = T, ...){
+auto_read <- function(file, reader = "", stringsAsFactors = F, remove.empty.columns = T, remove.empty.rows = T, ...){
   
-  if(endsWith(file,"xlsx") | endsWith(file, "xls")){
-    df <- readxl::read_excel(file, ...)
-  }else if (endsWith(file, "txt") | endsWith(file, "tsv")) {
-    df <- dplyr::as.tbl(read.delim(file, stringsAsFactors = stringsAsFactors, ...))
-  }else if (endsWith(file, "csv") ) {
-    df <- dplyr::as.tbl(read.csv(file, stringsAsFactors = stringsAsFactors, ...))
-  }else{ stop("file was not automatically recognizable type (xlsx, xls, txt, csv)") }
+  if( reader !=  "" ){                                    tool <- reader
+  }else if(endsWith(file,"xlsx") | endsWith(file, "xls")){tool <- "read_excel"
+  }else if(endsWith(file,"csv") ){                        tool <- "read.csv"
+  }else if(endsWith(file,"txt")  | endsWith(file, "tsv")){tool <- "read.delim"
+  }else{ stop("file reader was not set or an automatically recognized (xlsx, xls, txt, tsv, csv)") }
+  
+  if(       tool == "read_excel" ){ df <- readxl::read_excel(file, ...)
+  }else if( tool == "read.delim") { df <- dplyr::as.tbl(read.delim(file, stringsAsFactors = stringsAsFactors, ...))
+  }else if( tool == "read.csv")   { df <- dplyr::as.tbl(read.csv(file, stringsAsFactors = stringsAsFactors, ...))
+  }else if( tool == "fread")      { df <- data.table::fread(file, ...)
+  }else{ stop("tool not set") }
   
   # remove empty rows and colums
   if(remove.empty.columns) df <- df[, !apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )})]
-  if(remove.empty.rows)    df <- df[!apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )}), ] 
+  if(remove.empty.rows)    df <- df[!apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )}), ]
   df
 }
 
