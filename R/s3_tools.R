66404983767faa5d1fe34401668b8cd2782ec8a2
dkrozelle <dan.rozelle@gmail.com>
2017-05-03 17:31:27 -0400

    begin major revisions to s3 tools

--- R/s3_tools.R
+++ R/s3_tools.R
@@ -1,76 +1,102 @@
-#' Helper function for writing tab-delimited tables to an S3 bucket 
+#  -----------------------------------------------------------------------------
+#' Establish an environment which defines the current S3 bucket metadata.
 #'
-#' A helper function to build S3 path-specific tools. Since you will commonly write
-#' to a single path in an S3 bucket repeatedly, this simplifys the process by
-#' pre-populating longer arguments such as bucket and path strings.
+#' This environment is required for all of my S3... functions. Sets bucket name,
+#' defines and creates a local cache directory, tracks working directory
 #'
-#' @param bucket S3 bucket name
-#' @param path S3 prefix structure to prepend to the name, 
-#'                leading and trailing slashes are cleaned up.
-#' @param obj The Dataframe  you want to write
-#' @param name File name for the table
-#' @return Response code from AWS, "0" when successful
+#' @param bucket base name for s3 bucket e.g. s3://bucket
+#' @param wd character string path, optional to define a working prefix
+#' @param local.cache character string, path of a local scratch directory
+#'
+#' @return environment e returned invisibly to global environment
 #' @export
-WriteTableToS3 <- function(bucket, path = "", arguments = ""){
-  function(obj, name){
-    
-    base.name <- basename(name)
-    local.path <- file.path("/tmp",base.name)
-    
-    if(path == ""){
-      s3.path    <- file.path(gsub("\\/+$","",bucket), gsub("^\\/+|\\/+$","",name))
-    }else{
-    s3.path    <- file.path(gsub("\\/+$","",bucket), gsub("^\\/+|\\/+$","",path), gsub("^\\/+|\\/+$","",name))
-    }
-    
-    write.table(obj, local.path, row.names = F, col.names = T, sep = "\t", quote = F)
-    command.string <- paste('aws s3 cp', local.path, s3.path , '--sse',arguments , sep = " ")
-    print(command.string)
-    system( command.string )
-    response <- system('echo $?', intern = T)
-    if( response == 0 ){
-      unlink(local.path)
-    }else{
-      warning(paste("Error writing",name, "to S3", sep = " "))
-    }
-    response
-  }
+s3_set_env <- function(bucket        = NULL,
+                       wd          = NULL,
+                       local.cache = "/tmp/s3.cache"){
+  e <<- new.env(parent = emptyenv())
+  if( !is.null(bucket) ) e$bucket <- bucket
+
+  if( !is.null(wd) ) e$wd <- wd
+
+  e$local <- local.cache
+  if( !dir.exists(e$local) ) dir.create(e$local)
+  if( !dir.exists(e$local) ) stop('local scratch directory does not exist')
 }
-# f <- WriteTableToS3(bucket = "s3://celgene-helsinki-bucket", path = "AML")
-# df <- data.frame(one = c(1,2,3), two = c(1,2,3))
-# f(obj = df, name= "upload_tracking/test.txt")
-# 
-# 
 
-
-#' Read tables from S3 bucket directly into df object 
+#  -----------------------------------------------------------------------------
+#' Read tables from S3 bucket directly into df object
 #'
-#' Works for all delimited tables supported by toolboxR::Autoread().
-#' which includes csv, tab-delim, and xlsx). Only 
+#' Works for all delimited tables supported by toolboxR::auto_read().
+#' which includes csv, tab-delim, and xlsx). Only
 #'
-#' @param s3.path Full character string uri of the table. 
-#' @param cache logical value as to whether local file should be 
+#' @param s3.path Full character string uri of the table.
+#' @param cache logical value as to whether local file should be
 #'              left, default for cache=F removes file after transfer.
 #' @return data.frame
 #' @export
-GetS3Table <- function(s3.path, cache = F, aws.args = "", ...){
-  name  <- basename(s3.path)
-  local.filename <- file.path("/tmp", name)
-  system(  paste('aws s3 cp', s3.path, local.filename, aws.args, sep = " "))
-  df <- toolboxR::auto_read(local.filename, ...)
-  if(cache == FALSE){unlink(local.filename)}
-  df
-}
+s3_put_table <- function(table, pre, env = e, cache = F, aws.args = "", ...){
 
-#' Write tab-delim tables to S3 bucket 
-#'
-#' @param object a table object (df, data.table, tibble
-#' @param s3.path Full character string uri of the table. 
-#' @return aws response code
-#' @export
-PutS3Table <- function(object, s3.path, sep = "\t", row.names = F, quote = F, aws.args = "", ...){
+  if( is.null(env$bucket) ){stop('please set bucket environment: set.s3.bucket(bucket = "s3://bucket-name")')}
+
   name  <- basename(s3.path)
   local <- file.path("/tmp", name)
   write.table(object, local, sep = sep, row.names = row.names, quote = quote, ...)
   system(  paste('aws s3 cp', local, s3.path, "--sse", aws.args, sep = " "))
+}
+
+#  -----------------------------------------------------------------------------
+#' Read tables from S3 bucket directly into df object
+#'
+#' Works for all delimited tables supported by toolboxR::Autoread().
+#' which includes csv, tab-delim, and xlsx). Only
+#'
+#' @param s3.path Full character string uri of the table.
+#' @param cache logical value as to whether local file should be
+#'              left, default for cache=F removes file after transfer.
+#' @return data.frame
+#' @export
+s3_get_table <- function(pre, env = e, cache = F, aws.args = "", ...){
+return("in development")
+  # if( is.null(env$bucket) ){stop('please set bucket environment: set.s3.bucket(bucket = "s3://bucket-name")')}
+  #
+  # name    <- basename(pre)
+  # s3.path <- file.path(e$bucket, pre)
+  # local.filename <- file.path("/tmp", name)
+  # system(  paste('aws s3 cp', s3.path, local.filename, aws.args, sep = " "))
+  # df <- toolboxR::auto_read(local.filename, ...)
+  # if(cache == FALSE){unlink(local.filename)}
+  # df
+}
+
+s3write_using <- function(x, FUN, ..., object, bucket, opts = NULL) {
+    tmp <- tempfile()
+    on.exit(unlink(tmp))
+    value <- FUN(x, tmp, ...)
+    if (missing(bucket)) {
+        bucket <- get_bucketname(object)
+    }
+    object <- get_objectkey(object)
+    if (is.null(opts)) {
+        r <- put_object(file = tmp, bucket = bucket, object = object)
+    } else {
+        r <- do.call("put_object", c(list(file = rawConnectionValue(tmp), bucket = bucket, object = object), opts))
+    }
+    return(invisible(r))
+}
+
+
+s3read_using <- function(FUN, ..., object, bucket, opts = NULL) {
+
+    if (missing(bucket)) {
+        bucket <- get_bucketname(object)
+    }
+    object <- get_objectkey(object)
+
+    tmp <- tempfile(fileext = paste0(".", tools::file_ext(object)))
+    if (is.null(opts)) {
+        r <- save_object(bucket = bucket, object = object, file = tmp)
+    } else {
+        r <- do.call("save_object", c(list(bucket = bucket, object = object, file = tmp), opts))
+    }
+    return(FUN(tmp, ...))
 }
