a5603ba9c30e7317cc52f7c9e0fa2509456c391a
dkrozelle <dan.rozelle@gmail.com>
2017-05-30 09:49:52 -0400

    remove deprecates functions; incorporate new autoread

--- R/file_io.R
+++ R/file_io.R
@@ -1,87 +1,42 @@
+#' Automatic file reader
+#'
 #' Attempts to automatically recognize table type and import
-#'  as tibble. Latest version incorporates *dots* to facilitate
-#'  passing all possible options for each file reader type.
+#'  as tibble or data.table. Latest version incorporates *dots*
+#'  to facilitate passing all possible options for each file reader type.
 #'
 #' @param file path to an xlsx binary file
-#' @param reader Specify which package to read in your table, options include:
+#' @param reader string, Specify which package to read in your table, options include:
 #'              read_excel, read.delim, read.csv, fread. NOTE: fread option
 #'              will return a data.table, all others return data frame.
-#' @param remove.empty.columns,remove.empty.rows logical to remove columns or
+#' @param remove.empty.columns,remove.empty.rows logical, to remove columns or
 #'            rows composed entirely of NA, "NA", or blank values
+#' @param make.names.valid,make.names.unique logical, uses base make.names function
+#'            to clean table names and make unique.
 #' @param ... Pass any file-type appropriate arguments on to their handler
 #'            functions. e.g., readxl::read_excel(), read.delim(), read.csv()
 #' @return dataframe or data.table, see reader parameter.
 #' @export
-auto_read <- function(file, stringsAsFactors = F,
+auto_read <- function(file, reader = "", stringsAsFactors = F,
                       remove.empty.columns = F, remove.empty.rows = F,
                       make.names.valid = T, make.names.unique = T, ...){
 
-  if(endsWith(file,"xlsx") | endsWith(file, "xls")){
-    df <- readxl::read_excel(file, ...)
-  }else if (endsWith(file, "txt") | endsWith(file, "tsv")) {
-    df <- dplyr::as.tbl(read.delim(file, stringsAsFactors = stringsAsFactors, ...))
-  }else if (endsWith(file, "csv") ) {
-    df <- dplyr::as.tbl(read.csv(file, stringsAsFactors = stringsAsFactors, ...))
-  }else{ stop("file was not automatically recognizable type (xlsx, xls, txt, csv)") }
+  if( reader !=  "" ){                                    tool <- reader
+  }else if(endsWith(file,"xlsx") | endsWith(file, "xls")){tool <- "read_excel"
+  }else if(endsWith(file,"csv") ){                        tool <- "read.csv"
+  }else if(endsWith(file,"txt")  | endsWith(file, "tsv")){tool <- "read.delim"
+  }else{ stop("file reader was not set or an automatically recognized (xlsx, xls, txt, tsv, csv)") }
+
+  if(       tool == "read_excel" ){ df <- readxl::read_excel(file, ...)
+  }else if( tool == "read.delim") { df <- dplyr::as.tbl(read.delim(file, stringsAsFactors = stringsAsFactors, ...))
+  }else if( tool == "read.csv")   { df <- dplyr::as.tbl(read.csv(file, stringsAsFactors = stringsAsFactors, ...))
+  }else if( tool == "fread")      { df <- data.table::fread(file, ...)
+  }else{ stop("tool not set") }
 
   if(make.names.valid) names(df) <- make.names(names(df))
   if(make.names.unique) names(df) <- make.names(names(df), unique = T)
 
   # remove empty rows and colums
-  if(remove.empty.columns) df <- df[, !apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )})]
-  if(remove.empty.rows)    df <- df[!apply(df, MARGIN = 2, function(x){all( is.na(x) | x=="NA" | x == "" )}), ]
+  if(remove.empty.columns) df <- df[, !apply(df, 2, function(x){all( is.na(x) | x=="NA" | x == "" )})]
+  if(remove.empty.rows)    df <- df[!apply(df, 1, function(x){all( is.na(x) | x=="NA" | x == "" )}), ]
   df
 }
-
-
-#' Deprecated, use auto_read().
-#' Attempts to automatically recognize file type and import
-#'  as dataframe
-#'
-#' @param file path to an xlsx binary file
-#' @param ... Pass any file-type appropriate arguments on to their handler
-#'            functions. e.g., readxl::read_excel(), read.delim(), read.csv()
-#' @return dataframe
-#' @export
-AutoRead <- function(file, ...){
-  tryCatch({
-    if(endsWith(file,"xlsx") | endsWith(file, "xls")){
-      t <- readxl:::xlsx_col_types(file)
-      df <- readxl::read_excel(file, ...)
-      if(check.names){
-        df <- df[,!is.na(names(df))]
-        names(df) <- make.names(names(df), unique = TRUE)
-        }
-      df
-    }else if (endsWith(file, "txt")){
-      df <- read.delim(file = file,
-                       stringsAsFactors = F,
-                       header = colnames,
-                       check.names = check.names)
-      if(skip > 0){
-        names(df) <- df[skip+1,]
-        df <- df[(skip+1):nrow(df),]
-      }
-      # if(check.names){
-      #   df <- df[,!is.na(names(df))]
-      #   names(df) <- make.names(names(df), unique = TRUE)
-      # }
-
-      df
-
-    }else if (endsWith(file, "csv")){
-      df <- read.csv(file = file, stringsAsFactors = F, header = colnames)
-      if(skip > 0){
-        names(df) <- df[skip+1,]
-        df <- df[(skip+1):nrow(df),]
-      }
-      if(check.names){
-        df <- df[,!is.na(names(df))]
-        names(df) <- make.names(names(df), unique = TRUE)
-      }
-
-      df
-    }
-  },error=function(e){stop("File unable to be imported")})
-}
-
--- R/s3_tools.R
+++ R/s3_tools.R
@@ -1,106 +1,3 @@
-#  -----------------------------------------------------------------------------
-#' Establish an environment which defines the current S3 bucket metadata.
-#'
-#' This environment is required for all of my S3... functions. Sets bucket name,
-#' defines and creates a local cache directory, tracks working directory
-#'
-#' @param bucket base name for s3 bucket e.g. s3://bucket
-#' @param wd character string path, optional to define a working prefix
-#' @param local.cache character string, path of a local scratch directory
-#'
-#' @return environment e returned invisibly to global environment
-#' @export
-s3_set_env <- function(bucket        = NULL,
-                       wd          = NULL,
-                       local.cache = "/tmp/s3.cache"){
-  e <<- new.env(parent = emptyenv())
-  if( !is.null(bucket) ) e$bucket <- bucket
-
-  if( !is.null(wd) ) e$wd <- wd
-
-  e$local <- local.cache
-  if( !dir.exists(e$local) ) dir.create(e$local)
-  if( !dir.exists(e$local) ) stop('local scratch directory does not exist')
-}
-
-#  -----------------------------------------------------------------------------
-#' Read tables from S3 bucket directly into df object
-#'
-#' Works for all delimited tables supported by toolboxR::auto_read().
-#' which includes csv, tab-delim, and xlsx). Only
-#'
-#' @param s3.path Full character string uri of the table.
-#' @param cache logical value as to whether local file should be
-#'              left, default for cache=F removes file after transfer.
-#' @return data.frame
-#' @export
-s3_put_table <- function(table, pre, env = e, cache = F, aws.args = "", ...){
-
-  if( is.null(env$bucket) ){stop('please set bucket environment: set.s3.bucket(bucket = "s3://bucket-name")')}
-
-  name  <- basename(s3.path)
-  local <- file.path("/tmp", name)
-  write.table(object, local, sep = sep, row.names = row.names, quote = quote, ...)
-  system(  paste('aws s3 cp', local, s3.path, "--sse", aws.args, sep = " "))
-}
-
-#  -----------------------------------------------------------------------------
-#' Read tables from S3 bucket directly into df object
-#'
-#' Works for all delimited tables supported by toolboxR::Autoread().
-#' which includes csv, tab-delim, and xlsx). Only
-#'
-#' @param s3.path Full character string uri of the table.
-#' @param cache logical value as to whether local file should be
-#'              left, default for cache=F removes file after transfer.
-#' @return data.frame
-#' @export
-s3_get_table <- function(pre, env = e, cache = F, aws.args = "", ...){
-return("in development")
-  # if( is.null(env$bucket) ){stop('please set bucket environment: set.s3.bucket(bucket = "s3://bucket-name")')}
-  #
-  # name    <- basename(pre)
-  # s3.path <- file.path(e$bucket, pre)
-  # local.filename <- file.path("/tmp", name)
-  # system(  paste('aws s3 cp', s3.path, local.filename, aws.args, sep = " "))
-  # df <- toolboxR::auto_read(local.filename, ...)
-  # if(cache == FALSE){unlink(local.filename)}
-  # df
-}
-
-s3write_using <- function(x, FUN, ..., object, bucket, opts = NULL) {
-    tmp <- tempfile()
-    on.exit(unlink(tmp))
-    value <- FUN(x, tmp, ...)
-    if (missing(bucket)) {
-        bucket <- get_bucketname(object)
-    }
-    object <- get_objectkey(object)
-    if (is.null(opts)) {
-        r <- put_object(file = tmp, bucket = bucket, object = object)
-    } else {
-        r <- do.call("put_object", c(list(file = rawConnectionValue(tmp), bucket = bucket, object = object), opts))
-    }
-    return(invisible(r))
-}
-
-
-s3read_using <- function(FUN, ..., object, bucket, opts = NULL) {
-
-    if (missing(bucket)) {
-        bucket <- get_bucketname(object)
-    }
-    object <- get_objectkey(object)
-
-    tmp <- tempfile(fileext = paste0(".", tools::file_ext(object)))
-    if (is.null(opts)) {
-        r <- save_object(bucket = bucket, object = object, file = tmp)
-    } else {
-        r <- do.call("save_object", c(list(bucket = bucket, object = object, file = tmp), opts))
-    }
-    return(FUN(tmp, ...))
-}
-
 #' Read tables from S3 bucket directly into df object
 #'
 #' Works for all delimited tables supported by toolboxR::Autoread().
@@ -112,6 +9,8 @@ s3read_using <- function(FUN, ..., object, bucket, opts = NULL) {
 #' @return data.frame
 #' @export
 GetS3Table <- function(s3.path, cache = F, aws.args = "", ...){
+  message('DEPRECATED. this function will be removed in future versions,
+          please use new s3_get_table() method ')
   name  <- basename(s3.path)
   local.filename <- file.path("/tmp", name)
   system(  paste('aws s3 cp', s3.path, local.filename, aws.args, sep = " "))
@@ -127,6 +26,8 @@ GetS3Table <- function(s3.path, cache = F, aws.args = "", ...){
 #' @return aws response code
 #' @export
 PutS3Table <- function(object, s3.path, sep = "\t", row.names = F, quote = F, aws.args = "", ...){
+  message('DEPRECATED. this function will be removed in future versions,
+          please use new s3_put_table() method ')
   name  <- basename(s3.path)
   local <- file.path("/tmp", name)
   write.table(object, local, sep = sep, row.names = row.names, quote = quote, ...)
