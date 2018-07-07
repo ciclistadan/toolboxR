e458e9a660a878530dca3431902dec9ba90c3fbb
Dan Rozelle <dan.rozelle@ranchobiosciences.com>
2018-07-06 21:51:32 -0400

    roxygenize documentation

--- R/curate_df.R
+++ R/curate_df.R
@@ -1,42 +1,20 @@
 #############################################
-#' Renames specified columns in data.frame
+#' Sort specified columns to the front of a data frame
 #'
-#' @param old current column name
-#' @param new replacement column name
-#' @param df which data frame to replace it in
-#' @return A df with changed column names
-#' @export
-rename_columns <- function(old, new, df){
-  n <- names(df)
-  if(all(old %in% n)){
-    n <- gsub(old, new, n)
-    names(df) <- n
-    df
-  }
-  else warning("Column name not found")
-
-}
-
-#############################################
-#' Sort specified columns to front of a data.frame
-#'
-#' @param names a char vector of current columns you want
-#'              to move to the front of the df
-#' @param df which data frame to replace it in
-#' @param reverse [not yet implemented] move named columns
-#'                to end of df.
+#' @param df data.frame with existing columns
+#' @param ... Unquoted column names to place at front of df
+#' @param after Optional unquoted column name to place
+#'              specified columns immediately after
 #' @return A df with rearranged columns
 #' @export
-move_columns <- function(names, df, reverse = F){
-  n  <- names(df)
-
-  if(all(names %in% n)){
-    df <- df[,c(names, n[!(n %in% names)])]
-  } else {
-    warning("Column name not found")
-    }
-  return(df)
+select_to_front <- function(df, ..., after){
+  n <- rlang::enquos(...)
+  a <- dplyr::select(df, !!!n)
+  b <- dplyr::select(df, -(!!!n))
+  dplyr::bind_cols(a,b)
 }
+df <- data.frame(a=1:5, b=1:5,c=1:5, d=1:5,e=1:5, f=1:5 )
+select_to_front(df, f:e)
 
 
 #' Format columns names to lower, UPPER or Title.Case
@@ -87,23 +65,7 @@ format_column_names <- function(df,
   df
 }
 
-#############################################
-#' Sort specified columns to front of a data.frame
-#'
-#' @param names a char vector of current columns yto remove
-#' @param df data frame containing columns "names"
-#' @return A df with rearranged columns
-#' @export
-remove_columns <- function(names, df){
-  n  <- names(df)
 
-  if(all(names %in% n)){
-    df <- df[,c( n[ !(n %in% names) ])]
-  } else {
-    warning("Column name not found")
-  }
-  return(df)
-}
 
 
 #' Automated normalization of string formats
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # toolboxR
 
 A collection of convenience wrappers and simple functions for bioinformatics, clinical/biological data curation, and fileIO. 
+
+Version 1.0 is a the first major revision. The intent is to consistently implement tidy ethos of common table formats, vectorization, unquoted parameterization and pipeability. Many of my custom functions ahave also been generically implemented in the tidyverse removing the need to maintain them here (e.g. drop_column("col", df) is now accomplished with df %>% select(-col) ).
